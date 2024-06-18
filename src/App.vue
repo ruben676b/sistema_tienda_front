df3a001d616073af228f0ed98befdfffe0d0355f
rubenD <rubenllasacce69@gmail.com>
2024-06-17 20:10:25 -0500

    Se añadio pokemonsView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/pokemons">Pokemons</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Pokemons</h1>
+    </div>
+</template>
