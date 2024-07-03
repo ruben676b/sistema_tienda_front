dca24d5818a4d25b47ad5fcad1d6913c2068a821
rubenD <rubenllasacce69@gmail.com>
2024-07-03 12:15:46 -0500

    Se agrego elegir formas de pago

--- src/components/VentaAdd.vue
+++ src/components/VentaAdd.vue
@@ -16,10 +16,8 @@
                   @update:busquedaProducto="busquedaProducto = $event" />
             </div>
 
-            <ListadoProductos :productosFiltrados="productosFiltrados" 
-               :isProductoSeleccionado="isProductoSeleccionado"
-               :agregarProducto="agregarProducto" 
-               :getProveedorImage="getProveedorImage" />
+            <ListadoProductos :productosFiltrados="productosFiltrados" :isProductoSeleccionado="isProductoSeleccionado"
+               :agregarProducto="agregarProducto" :getProveedorImage="getProveedorImage" />
          </div>
 
          <br><br>
@@ -30,14 +28,9 @@
                   <TotalArticulos :productosSeleccionados="productosSeleccionados"
                      @limpiar-productos="limpiarProductos" />
                   <div class="product-table">
-                     <ProductoSeleccionado 
-                        v-for="producto in productosSeleccionados" 
-                        :key="producto.idProducto"
-                        :producto="producto" 
-                        :getProveedorImage="getProveedorImage" 
-                        @editar-producto="editarProducto"
-                        @decrementar-cantidad="decrementarCantidad" 
-                        @incrementar-cantidad="incrementarCantidad"
+                     <ProductoSeleccionado v-for="producto in productosSeleccionados" :key="producto.idProducto"
+                        :producto="producto" :getProveedorImage="getProveedorImage" @editar-producto="editarProducto"
+                        @decrementar-cantidad="decrementarCantidad" @incrementar-cantidad="incrementarCantidad"
                         @eliminar-producto="eliminarProducto" />
                   </div>
                </div>
@@ -50,15 +43,15 @@
                      <ul>
                         <li>
                            <h5>Subtotal</h5>
-                           <h6>{{ formatearPrecio(subtotal)  }}</h6>
+                           <h6>{{ formatearPrecio(subtotal) }}</h6>
                         </li>
                         <li>
                            <h5>Impuesto</h5>
-                           <h6>{{ formatearPrecio(impuesto)  }}</h6>
+                           <h6>{{ formatearPrecio(impuesto) }}</h6>
                         </li>
                         <li class="total-value">
                            <h5>Total</h5>
-                           <h6>{{ formatearPrecio(total)  }}</h6>
+                           <h6>{{ formatearPrecio(total) }}</h6>
                         </li>
                      </ul>
                   </div>
@@ -90,6 +83,26 @@
                </div>
                <div v-if="tipoCliente === 'natural'">
                   <ClienteNatural />
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6">
+               <div class="form-group">
+                  <label for="formaPago">Forma de pago</label>
+                  <select v-model="formaPagoSeleccionada" class="form-control" id="formaPago">
+                     <option value="">Seleccione una forma de pago</option>
+                     <option v-for="formaPago in formasPago" :key="formaPago.id" :value="formaPago.id">
+                        {{ formaPago.Descripcion }}
+                     </option>
+                  </select>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <br>
+               <div class="btn-totallabel">
+                  <h5>Checkout</h5>
+                  <h6>60.00$</h6>
                </div>
             </div>
          </div>
@@ -134,6 +147,8 @@ export default {
          selectedComponent: null,
          busquedaProducto: '',
          productosSeleccionados: [],
+         formasPago: [],
+         formaPagoSeleccionada: '',
          subtotal: 0,
          impuesto: 0,
          total: 0
@@ -142,6 +157,7 @@ export default {
 
    created() {
       this.obtenerProductos();
+      this.obtenerFormasPago();
    },
    computed: {
       totalVenta() {
@@ -182,6 +198,20 @@ export default {
       },
    },
    methods: {
+      async obtenerFormasPago() {
+         try {
+            const response = await axios.get('http://localhost:3000/api/v1/formas-pago');
+            if (response.data.success) {
+               this.formasPago = response.data.formasPago || [];
+            } else {
+               console.error('Error al obtener formas de pago:', response.data.message);
+               this.formasPago = [];
+            }
+         } catch (error) {
+            console.error('Error al obtener formas de pago:', error);
+            this.formasPago = [];
+         }
+      },
       async obtenerProductos() {
          try {
             const response = await axios.get('http://localhost:3000/api/v1/productos');
@@ -204,8 +234,8 @@ export default {
          this.subtotal = this.total - this.impuesto;
       },
       formatearPrecio(precio) {
-  return precio.toFixed(2);
-},
+         return precio.toFixed(2);
+      },
       isProductoSeleccionado(producto) {
          return this.productosSeleccionados.some(p => p.IdProducto === producto.IdProducto);
       },
@@ -251,21 +281,7 @@ export default {
       getProveedorImage(imagePath) {
          return imagePath ? `http://localhost:3000/api/v1/uploads/productos/${imagePath}` : "../../public/img/product/noimage.png";
       },
-      openClientModal() {
-         Swal.fire({
-            title: 'Seleccione el tipo de cliente',
-            showDenyButton: true,
-            showCancelButton: true,
-            confirmButtonText: 'Cliente Natural',
-            denyButtonText: 'Cliente Jurídico',
-         }).then((result) => {
-            if (result.isConfirmed) {
-               this.$router.push({ name: 'ClienteAddNatural' });
-            } else if (result.isDenied) {
-               this.$router.push({ name: 'ClienteAddJuridico' });
-            }
-         });
-      },
+
    },
 };
 </script>
